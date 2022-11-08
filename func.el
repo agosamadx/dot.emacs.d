945161db8c9617fbab0335ce730224f9493699cc
ago <ago@autoexec.jp>
2022-11-08 15:51:29 +0900

    Initial commit

---
+++ func.el
@@ -0,0 +1,72 @@
+;;; scratch復活
+(defun make-scratch (&optional arg)
+  (interactive)
+  (progn
+    (set-buffer (get-buffer-create "*scratch*"))
+    (funcall initial-major-mode)
+    (erase-buffer)
+    (when (and initial-scratch-message (not inhibit-startup-message))
+      (insert initial-scratch-message))
+    (or arg (progn (setq arg 0)
+                   (switch-to-buffer "*scratch*")))
+    (cond ((= arg 0) (message "*scratch* is cleared up."))
+          ((= arg 1) (message "another *scratch* is created")))))
+
+;;; 行頭とインデント末をトグル
+(defun beggining-of-indented-line (point)
+  (interactive "d")
+  (if (string-match
+       "^[ \t]+$"
+       (save-excursion
+         (buffer-substring-no-properties
+          (progn (beginning-of-line) (point))
+          point)))
+      (beginning-of-line)
+    (back-to-indentation)))
+
+;;; 行頭のスペースをtab-width分ずつ消す
+(defun hungry-backspace ()
+  (interactive)
+  (let* ((current (- (point) (line-beginning-position)))
+         (div (% current tab-width))
+         (nextperiod (min (+ (point) (- tab-width (if (= div 0) tab-width div))) (line-end-position))))
+    (if (and (string-match "^\s+$" (buffer-substring-no-properties (line-beginning-position) nextperiod))
+             (not (= (point) (line-beginning-position)))
+             (= (% (- nextperiod (line-beginning-position)) tab-width) 0))
+        (backward-delete-char (if (= (% current tab-width) 0) tab-width (% current tab-width)))
+      (backward-delete-char 1))))
+
+;;; スペースが続いている時はtab-widthずつ消す
+(defun hungry-delete ()
+  (interactive)
+  (let* ((current (- (point) (line-beginning-position)))
+         (step (- tab-width (% current tab-width)))
+         (nextperiod (min (+ (point) step) (line-end-position))))
+    (if (and (string-match "^\s+$" (buffer-substring-no-properties (line-beginning-position) nextperiod))
+             (not (= (point) (line-end-position)))
+             (= (% (- nextperiod (line-beginning-position)) tab-width) 0))
+        (delete-char step)
+      (delete-char 1))))
+
+;;; スペースが続いている時はタブ幅ごとまとめて移動する
+(defun hungry-forward-char ()
+  (interactive)
+  (let* ((current (- (point) (line-beginning-position)))
+         (nextperiod (min (+ (point) (- tab-width (% current tab-width))) (line-end-position))))
+    (if (and (string-match "^\s+$" (buffer-substring-no-properties (line-beginning-position) nextperiod))
+             (not (= (point) (line-end-position)))
+             (= (% (- nextperiod (line-beginning-position)) tab-width) 0))
+        (forward-char (- tab-width (% current tab-width)))
+      (forward-char 1))))
+
+;;; スペースが続いている時はタブ幅ごとまとめて移動する
+(defun hungry-backward-char ()
+  (interactive)
+  (let* ((current (- (point) (line-beginning-position)))
+         (div (% current tab-width))
+         (nextperiod (min (+ (point) (- tab-width (if (= div 0) tab-width div))) (line-end-position))))
+    (if (and (string-match "^\s+$" (buffer-substring-no-properties (line-beginning-position) nextperiod))
+             (not (= (point) (line-beginning-position)))
+             (= (% (- nextperiod (line-beginning-position)) tab-width) 0))
+        (backward-char (if (= (% current tab-width) 0) tab-width (% current tab-width)))
+      (backward-char 1))))
---
+++ init.el
@@ -0,0 +1,171 @@
+;(setq debug-on-error t)
+
+;;; package.el
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; install packages
+(defvar package-list '(company undo-tree yasnippet ddskk lsp-mode lsp-ui flycheck cmake-mode csharp-mode))
+(unless package-archive-contents (package-refresh-contents))
+(dolist (pkg package-list)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
+;;; setting
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message nil)
+(setq completion-ignore-case t)
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq kill-whole-line t)
+(setq visible-bell t)
+(setq ring-bell-function 'ignore)
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+(setq-default truncate-lines t)
+(custom-set-variables '(custom-file (expand-file-name "custom.el" user-emacs-directory)))
+
+;;; func
+(load-file (locate-user-emacs-file "func.el"))
+
+;;; for mac
+(when (memq window-system '(mac ns))
+  (load-file (locate-user-emacs-file "mac.el")))
+
+;;; for windows
+(when window-system '(windows-nt)
+      (load-file (locate-user-emacs-file "windows.el")))
+
+;;; frame
+(defvar frame-parameters
+  '((width . 180)
+    (height . 50)
+    (top . 64)
+    (left . 192)
+    (tool-bar-lines . nil)))
+
+(when (memq window-system '(x w32 mac ns))
+  (set-scroll-bar-mode nil)
+  (setq frame-title-format '(multiple-frames "%b" ("" invocation-name)))
+  (setq default-frame-alist frame-parameters))
+
+;;; theme
+(setq custom-theme-directory (locate-user-emacs-file "themes/"))
+(load-theme 'custom t)
+
+;;; paren-mode
+(show-paren-mode t)
+(setq show-paren-delay 0)
+(setq show-paren-style 'mixed)
+
+(global-set-key "\C-h" 'backward-delete-char)
+(global-set-key "\C-ce" 'eshell)
+(global-set-key "\C-x\C-b" 'electric-buffer-list)
+(global-set-key "\C-a" 'beggining-of-indented-line)
+(global-set-key "\C-k" 'kill-line)
+(global-set-key "\M-k" (lambda () (interactive) (kill-line 0)))
+(add-hook 'electric-buffer-menu-mode-hook
+          (lambda()
+            (local-set-key "x" 'Buffer-menu-execute)))
+
+;;; company
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(setq company-minimum-prefix-length 2)
+(setq company-selection-wrap-around t)
+(setq completion-ignore-case t)
+(define-key company-active-map (kbd "TAB") 'nil)
+(define-key company-active-map (kbd "C-m") 'company-complete-selection)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-h") nil)
+(define-key company-active-map (kbd "M-n") nil)
+(define-key company-active-map (kbd "M-p") nil)
+(define-key company-search-map (kbd "C-n") 'company-select-next)
+(define-key company-search-map (kbd "C-p") 'company-select-previous)
+(define-key company-search-map (kbd "M-n") nil)
+(define-key company-search-map (kbd "M-p") nil)
+
+;;; lsp
+(setq lsp-prefer-capf t)
+(setq lsp-enable-on-type-formatting nil)
+
+;;; redo-tree
+(require 'undo-tree nil t)
+(global-undo-tree-mode)
+(setq undo-tree-auto-save-history nil)
+(global-set-key (kbd "C-M-/") 'undo-tree-redo)
+
+(require 'cmake-mode nil t)
+(setq auto-mode-alist
+      (append
+       '(("CMakeLists\\.txt$" . cmake-mode)
+         ("\\.cmake$" . cmake-mode))
+       auto-mode-alist))
+
+;; yasnippet
+(require 'yasnippet nil t)
+(yas-global-mode)
+
+;;; ddskk
+(when (require 'skk nil t)
+  (require 'skk-study)
+  (setq default-input-method "japanese-skk")
+  (add-hook 'isearch-mode-hook 'skk-isearch-mode-setup)
+  (add-hook 'isearch-mode-end-hook 'skk-isearch-mode-cleanup)
+  (setq skk-isearch-start-mode 'latin))
+
+;;; c-mode
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (setq c-default-style "stroustrup")
+            (setq c-basic-offset 4)
+            (setq tab-width 4)
+            (setq indent-tabs-mode nil)
+            (local-set-key "\C-h" 'hungry-backspace)
+            (local-set-key "\C-d" 'hungry-delete)
+            (local-set-key "\C-f" 'hungry-forward-char)
+            (local-set-key "\C-b" 'hungry-backward-char)))
+
+;;; c++-mode
+(setq auto-mode-alist
+      (append
+       '(("\\.h$" . c++-mode)
+         ("\\.hpp$" . c++-mode))
+       auto-mode-alist))
+(add-hook 'c++-mode-hook
+          (lambda()
+            (c-set-offset 'innamespace 0)
+            (c-set-offset 'inlambda 0)
+            (c-set-offset 'arglist-close 0)
+            (c-set-offset 'arglist-cont-nonempty 0)))
+(add-hook 'c++-mode-hook 'company-mode)
+(add-hook 'c++-mode-hook 'flycheck-mode)
+(add-hook 'c++-mode-hook #'lsp)
+
+;;; csharp-mode
+(add-hook 'csharp-mode-hook #'lsp)
+
+;;; white space
+(require 'whitespace)
+(setq whitespace-style
+      '(face
+        trailing
+        tabs
+        space-mark
+        tab-mark
+        empty))
+(set-face-attribute
+ 'whitespace-tab nil
+ :background nil
+ :foreground nil
+ :underline nil)
+(set-face-attribute
+ 'whitespace-empty nil
+ :background nil)
+(setq whitespace-display-mappings
+      '((tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+(setq whitespace-action
+      '(auto-cleanup))
+(add-hook 'c++-mode-hook 'whitespace-mode)
