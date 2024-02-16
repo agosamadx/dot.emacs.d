17334e8a77193667e8fdbf42720bcfd85fbc142f
ago <ago@autoexec.jp>
2024-02-16 09:40:39 +0000

    theme設定

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 (require 'use-package)
 
 ;; install packages
-(defvar package-list '(company undo-tree yasnippet ddskk lsp-mode lsp-ui flycheck cmake-mode csharp-mode zenburn-theme))
+(defvar package-list '(company undo-tree yasnippet ddskk lsp-mode lsp-ui flycheck cmake-mode csharp-mode spacemacs-theme))
 (unless package-archive-contents (package-refresh-contents))
 (dolist (pkg package-list)
   (unless (package-installed-p pkg)
@@ -28,7 +28,7 @@
 (custom-set-variables '(custom-file (expand-file-name "custom.el" user-emacs-directory)))
 
 ;;; theme
-(load-theme 'zenburn t)
+(load-theme 'spacemacs-dark t)
 
 ;;; func
 (load-file (locate-user-emacs-file "func.el"))
