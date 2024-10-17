f02923bb5bd26e3379fda0d77764c8a191d35cbe
ago <ago@autoexec.jp>
2024-10-17 12:57:20 +0900

    update

--- init.el
+++ init.el
@@ -55,42 +55,6 @@
 (setq show-paren-delay 0)
 (setq show-paren-style 'mixed)
 
-;;; ddskk
-(use-package skk
-  :commands
-  (skk skk-study)
-  :config
-  (progn
-    (setq default-input-method "japanese-skk")
-    (add-hook 'isearch-mode-hook 'skk-isearch-mode-setup)
-    (add-hook 'isearch-mode-end-hook 'skk-isearch-mode-cleanup)
-    (setq skk-isearch-start-mode 'latin))
-  :bind (("C-\\" . skk-mode)))
-
-;;; c-mode
-(add-hook 'c-mode-common-hook
-          (lambda()
-            (c-set-style "stroustrup")
-            (c-set-offset 4)
-            (local-set-key "\C-h" 'hungry-backspace)
-            (local-set-key "\C-d" 'hungry-delete)
-            (local-set-key "\C-f" 'hungry-forward-char)
-            (local-set-key "\C-b" 'hungry-backward-char)))
-
-;;; c++-mode
-(setq auto-mode-alist
-      (append
-       '(("\\.h$" . c++-mode)
-         ("\\.hpp$" . c++-mode))
-       auto-mode-alist))
-(add-hook 'c++-mode-hook
-          (lambda()
-            (c-set-offset 'innamespace 0)
-            (c-set-offset 'inlambda 0)
-            (c-set-offset 'arglist-intro '+)
-            (c-set-offset 'arglist-close 0)
-            (c-set-offset 'arglist-cont-nonempty 0)))
-
 ;;; lsp-mode
 (use-package lsp-mode
   :ensure t
@@ -125,6 +89,43 @@
   :config
   (progn
     (yas-global-mode)))
+
+;;; ddskk
+(use-package ddskk
+  :ensure t
+  :commands
+  (skk skk-study)
+  :config
+  (progn
+    (setq default-input-method "japanese-skk")
+    (setq-default skk-isearch-start-mode 'latin)
+    (add-hook 'isearch-mode-hook 'skk-isearch-mode-setup)
+    (add-hook 'isearch-mode-end-hook 'skk-isearch-mode-cleanup))
+  :bind (("C-\\" . skk-mode)))
+
+;;; c-mode
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (c-set-style "stroustrup")
+            (c-set-offset 4)
+            (local-set-key "\C-h" 'hungry-backspace)
+            (local-set-key "\C-d" 'hungry-delete)
+            (local-set-key "\C-f" 'hungry-forward-char)
+            (local-set-key "\C-b" 'hungry-backward-char)))
+
+;;; c++-mode
+(setq auto-mode-alist
+      (append
+       '(("\\.h$" . c++-mode)
+         ("\\.hpp$" . c++-mode))
+       auto-mode-alist))
+(add-hook 'c++-mode-hook
+          (lambda()
+            (c-set-offset 'innamespace 0)
+            (c-set-offset 'inlambda 0)
+            (c-set-offset 'arglist-intro '+)
+            (c-set-offset 'arglist-close 0)
+            (c-set-offset 'arglist-cont-nonempty 0)))
 
 (provide 'init)
 ;;; init.el ends here
